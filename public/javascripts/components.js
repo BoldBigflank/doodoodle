8419c7d74ea7744604d86023138fa44d96b93359
Alex Swan <alex@bold-it.com>
2015-04-04 00:41:19 -0700

    Proper mouse drawing

--- public/javascripts/components.js
+++ public/javascripts/components.js
@@ -1,8 +1,9 @@
 angular.module('components', [])
-  .directive("drawing", function(){
+  .directive("drawing", function($document){
     return {
       restrict: "A",
       link: function(scope, element){
+        var canvas = element[0]
         var ctx = element[0].getContext('2d');
 
         // variable that decides if something should be drawn on mousemove
@@ -12,89 +13,71 @@ angular.module('components', [])
         var lastX;
         var lastY;
 
-        // drawing.setAttribute('width', element[0].parent().width());
-        // drawing.setAttribute('height', element[0].parent().height());
-        // drawing.setAttribute('id', 'canvas');
-        
-        // *** Mouse Controls ***
-        element.bind('mousedown', function(event){
+        var start = function(event){
           if(event.offsetX!==undefined){
-            lastX = event.offsetX;
-            lastY = event.offsetY;
+            console.log("offsetX")
+            coord = canvasCoord({"x":event.x, "y":event.y})
           } else { // Firefox compatibility
-            lastX = event.layerX - event.currentTarget.offsetLeft;
-            lastY = event.layerY - event.currentTarget.offsetTop;
+            console.log("layerX")
+            coord = canvasCoord({"x":event.layerX - event.currentTarget.offsetLeft, "y":event.layerY - event.currentTarget.offsetTop})
           }
+
+          lastX = coord.x;
+          lastY = coord.y;
 
           // begins new line
           ctx.beginPath();
-
           drawing = true;
-        });
-        element.bind('mousemove', function(event){
+        }
+
+        var move = function(event){
           event.preventDefault();
           if(drawing){
             // get current mouse position
             if(event.offsetX!==undefined){
-              currentX = event.offsetX;
-              currentY = event.offsetY;
+              currentX = event.x;
+              currentY = event.y;
             } else {
               currentX = event.layerX - event.currentTarget.offsetLeft;
               currentY = event.layerY - event.currentTarget.offsetTop;
             }
 
-            draw(lastX, lastY, currentX, currentY);
+            coord = canvasCoord({"x":currentX, "y":currentY})
+            
+            draw(lastX, lastY, coord.x, coord.y);
 
             // set current coordinates to last one
-            lastX = currentX;
-            lastY = currentY;
+            lastX = coord.x;
+            lastY = coord.y;
           }
 
-        });
-        element.bind('mouseup', function(event){
+        }
+
+        var end = function(event){
           // stop drawing
           drawing = false;
-        });
+        }
+
+        var canvasCoord = function(coord){
+          // Modify it based on its scale
+          coord.x -= canvas.getBoundingClientRect().left
+          coord.y -= canvas.getBoundingClientRect().top
+          
+          // Modify it based on the canvas's scale
+          coord.x *= canvas.width / parseInt(canvas.style.width, 10);
+          coord.y *= canvas.height / parseInt(canvas.style.height, 10);
+          return coord;
+        }
+
+        // *** Mouse Controls ***
+        element.bind('mousedown', start);
+        element.bind('mousemove', move);
+        $document.bind('mouseup', end);
 
         // *** Touch Controls ***
-        element.bind('touchstart', function(event){
-          if(event.offsetX!==undefined){
-            lastX = event.offsetX;
-            lastY = event.offsetY;
-          } else { // Firefox compatibility
-            lastX = event.touches[0].pageX - event.currentTarget.offsetLeft;
-            lastY = event.touches[0].pageY - event.currentTarget.offsetTop;
-          }
-
-          // begins new line
-          ctx.beginPath();
-
-          drawing = true;
-        });
-        element.bind('touchmove', function(event){
-          event.preventDefault();
-          if(drawing){
-            // get current mouse position
-            if(event.offsetX!==undefined){
-              currentX = event.touches[0].pageX;
-              currentY = event.touches[0].pageY;
-            } else {
-              currentX = event.touches[0].pageX - event.currentTarget.offsetLeft;
-              currentY = event.touches[0].pageY - event.currentTarget.offsetTop;
-            }
-
-            draw(lastX, lastY, currentX, currentY);
-
-            // set current coordinates to last one
-            lastX = currentX;
-            lastY = currentY;
-          }
-
-        });
-        element.bind('touchend', function(event){
-          // stop drawing
-          drawing = false;
-        });
+        element.bind('touchstart', start);
+        element.bind('touchmove', move);
+        element.bind('touchend', end);
 
         // canvas reset
         function reset(){
@@ -109,10 +92,36 @@ angular.module('components', [])
           // color
           ctx.strokeStyle = "#df4b26";
           ctx.lineJoin = "round";
+          ctx.lineCap = 'round';
           ctx.lineWidth = 5;
           // draw it
           ctx.stroke();
         }
       }
     };
-  });
+  })
+  .directive('resize', function ($window) {
+    return function (scope, element) {
+      var ctx = element[0].getContext('2d');
+      scope.$watch(function () {
+        return { 'h': $window.innerHeight, 'w': $window.innerWidth };
+      }, function (newValue, oldValue) {
+        scope.windowHeight = newValue.h;
+        scope.windowWidth = newValue.w;
+
+        scope.style = function () {
+          var newHeight = newValue.h - 100
+          ctx.scale(newValue.w/oldValue.w, newValue.h/oldValue.h)
+        return { 
+            'height': newHeight + 'px',
+            'width': (0.75 * newHeight) + 'px' 
+          };
+        };
+              
+      }, true);
+    
+      angular.element($window).bind('resize', function () {
+        scope.$apply();
+      });
+    }
+  })
